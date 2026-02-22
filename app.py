0348576fcfcf89f63718bd7f9c1991fd22c0d77b
BISWANATH  MAHAPATRA <78414378+biswanath123456@users.noreply.github.com>
2026-02-22 16:44:18 +0530

    Hosting
    Changes in requirements.txt
    added gunicorn
    Changes in app

--- app.py
+++ app.py
@@ -48,5 +48,7 @@ def reset():
     return jsonify({'success': True})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
     
